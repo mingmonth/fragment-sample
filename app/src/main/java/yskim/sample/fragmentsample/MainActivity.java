6ec62d4574036df9484065b643da0241d2c613ad
mingmonth <ys9922@naver.com>
2019-12-11 08:16:00 +0900

    버튼 클릭 이벤트

--- app/src/main/java/yskim/sample/fragmentsample/MainActivity.java
+++ app/src/main/java/yskim/sample/fragmentsample/MainActivity.java
@@ -1,22 +1,62 @@
 package yskim.sample.fragmentsample;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.os.Bundle;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.button1)
+    Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         FragmentManager fm = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fm.beginTransaction();
         fragmentTransaction.add(R.id.fragmentBorC, new FragmentB());
         fragmentTransaction.commit();
 
+    }
+
+    @OnClick(R.id.button1)
+    public void onViewClicked(View view) {
+        switch(view.getId()) {
+            case R.id.button1:
+                switchFragment();
+                break;
+        }
+
+    }
+
+    private boolean isFragmentB = true;
+
+    private void switchFragment() {
+        Fragment fr;
+
+        if(isFragmentB) {
+            fr = new FragmentC();
+        } else {
+            fr = new FragmentB();
+        }
+
+        isFragmentB = (isFragmentB) ? false : true;
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentBorC, fr);
+        fragmentTransaction.commit();
     }
 }
